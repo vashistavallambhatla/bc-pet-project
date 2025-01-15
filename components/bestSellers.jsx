e2c17aa08e449394b93f7bc4a25ed9bb4810d1a6
vashistavallambhatla <vashista.vallambhatla@gmail.com>
2025-01-15 16:58:17 +0530

    loading-homepage

--- components/bestSellers.jsx
+++ components/bestSellers.jsx
@@ -1,51 +1,12 @@
 import { Container,Box,Typography } from "@mui/material"
 import Card from "../components/card.jsx"
 import { homePageHeadings,homePageLeftBox } from "../src/commonStyles.js"
-import { useEffect,useState } from "react"
-import supabase from "../supabase/supabaseClient.js"
 
+const BestSellers = ({ bestsellers }) => {
 
-const BestSellers = () => {
-
-    const [bestsellers,setBestsellers] = useState([])
-
-    const bestSellers = [
-        {   
-            id : 1,
-            image_url : "https://www.thirdwavecoffeeroasters.com/cdn/shop/products/1_e49e09b5-6de2-43ce-82a9-ae5a03f386e4_1200x1200.jpg?v=1672991807",
-            name : "EL DIABLO",
-            roast_type : "medium dark",
-            profile : "Orange,Zesty and Bold"
-        },
-        {   
-            id : 5,
-            image_url : "https://www.thirdwavecoffeeroasters.com/cdn/shop/products/1_37109c38-db66-4c29-a5c9-370a5dc3a573_1200x1200.jpg?v=1673341767",
-            name : "VIENNA ROAST",
-            roast_type : "dark roast",
-            profile : "Dark Chocolate, Maple Syrup, Toasted Walnut"
-        },
-        {   
-            id : 2,
-            image_url : "https://www.thirdwavecoffeeroasters.com/cdn/shop/files/SSOBABACOFFEEBEANFRONT_1200x1200.jpg?v=1694725258",
-            name : "Speciality Single Origin Bababudangiri Region",
-            roast_type : "medium light roast",
-            profile : "Pineapple, Passion Fruit, Molasses"
-        }
-    ]
-
-    useEffect(()=>{
-        const getBestsellers = async () => {
-            try {
-                const {data,error} = await supabase.from("products").select("*").eq("bestseller","TRUE")
-                if(error) throw new Error("Error while fetching bestsellers",error.message)
-                console.log(data)
-                setBestsellers(data)
-            } catch(error) {
-                console.error(error)
-            }
-        } 
-        getBestsellers()
-    },[])
+    if (!bestsellers || bestsellers.length === 0) {
+        return null;
+    }
     
     return (
         <Container maxWidth={false} sx={{display : "flex",fontFamily : "Raleway",marginTop : "5rem",marginBottom : "5rem",p: 0, 
--- pages/homePage.jsx
+++ pages/homePage.jsx
@@ -1,13 +1,41 @@
-import { Container, Typography , Box} from "@mui/material"
+import { Container } from "@mui/material"
 import BestSellers from "../components/bestSellers"
 import CoffeeForEveryOne from "../components/coffeeForEveryone"
+import { ClipLoader } from "react-spinners"
+import { useState,useEffect } from "react"
+import supabase from "../supabase/supabaseClient"
 
 
 const HomePage = () => {
+    const [loading,setLoading] = useState(true)
+    const [bestsellers,setBestsellers] = useState([])
+
+    useEffect(()=>{
+        const getBestsellers = async () => {
+            try {
+                const {data,error} = await supabase.from("products").select("*").eq("bestseller","TRUE")
+                if(error) throw new Error("Error while fetching bestsellers",error.message)
+                setBestsellers(data)
+            } catch(error) {
+                console.error(error)
+            } finally {
+                setLoading(false)
+            }
+        } 
+        getBestsellers()
+    },[])
+
+    if (loading) {
+        return (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <ClipLoader />
+          </div>
+        );
+    }
 
     return (
         <Container maxWidth={false} >
-            <BestSellers/>
+            <BestSellers bestsellers={bestsellers}/>
             <CoffeeForEveryOne/>
         </Container>
     )
